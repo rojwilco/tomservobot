69993758f75730ef62490de9fca92623f255bf20
rojwilco <roj@moron.ai>
2023-10-19 17:29:57 -0400

    cleanup, cogs
    remove autoreload.py
    experimenting with cogs

--- bot.py
+++ bot.py
@@ -1,34 +1,56 @@
+import asyncio
 import os
 
-import discord
+from discord import (
+    Intents
+)
+from discord.ext import commands
 from dotenv import load_dotenv
 
-load_dotenv()
-TOKEN = os.getenv('DISCORD_TOKEN')
+class Greetings(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
 
-intents = discord.Intents.default()
-intents.typing = False
-intents.presences = False
-intents.message_content = True
-
-client = discord.Client(intents=intents)
-
-@client.event
-async def on_ready():
-    print(f'{client.user} has connected to Discord with intents {[intent for intent in intents]}')
-    print(f'{client.user} is connected to the following servers:')
-    for guild in client.guilds:
-        print(f'- {guild.name} ({guild.id})')
-
-@client.event
-async def on_message(message):
-    print(f'processing message: {message.content}')
-    # don't respond to myself
-    if message.author == client.user:
-        return
-
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello there!!')
+    @commands.command()
+    async def hello(self, ctx, *, member = None):
+        """Says hello"""
+        member = member or ctx.author
+        await ctx.send(f'Well, hey there, {member.name}!')
 
 
-client.run(TOKEN)
+async def main():
+    load_dotenv()
+    TOKEN = os.getenv('DISCORD_TOKEN')
+
+    intents = Intents.default()
+    intents.typing = False
+    intents.presences = False
+    intents.message_content = True
+
+    bot = commands.Bot(command_prefix="$", intents=intents)
+    print(f'Loading Cogs...')
+    await bot.add_cog(Greetings(bot))
+
+    @bot.event
+    async def on_ready():
+        print(f'{bot.user} has connected to Discord with intents {[intent for intent in intents]}')
+        print(f'{bot.user} is connected to the following servers:')
+        for guild in bot.guilds:
+            print(f'- {guild.name} ({guild.id})')
+
+    # @bot.event
+    # async def on_message(message):
+    #     print(f'processing message: {message.content}')
+    #     # don't respond to myself
+    #     if message.author == bot.user:
+    #         return
+
+    #     if message.content.startswith('$hello2'):
+    #         await message.channel.send('Hello there!!')
+
+
+
+    await bot.start(TOKEN)
+
+if __name__ == "__main__":
+    asyncio.run(main())
