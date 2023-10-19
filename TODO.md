d55308ff171bf3209c66d765fc50d2d5779cb2ea
rojwilco <roj@moron.ai>
2023-10-19 13:56:49 -0400

    personality, todo
    modifies prompt to use a personality
    adds a TODO list

---
+++ TODO.md
@@ -0,0 +1,10 @@
+# TODO
+
+## Code stuff
+   - [ ] use `memory` param instead of stuffing prompt with history
+   - [ ] parameterize personality
+   - [ ] importable lib for bot
+   - [ ] hook to bot!  (cogs?)
+
+## Research
+   - Fine tuning
--- qa.py
+++ qa.py
@@ -11,6 +11,7 @@ from langchain.embeddings import OpenAIEmbeddings
 from langchain.vectorstores import Chroma
 from langchain.chat_models import ChatOpenAI
 from langchain.chains import ConversationalRetrievalChain
+from langchain.prompts import PromptTemplate
 from langchain.text_splitter import CharacterTextSplitter
 
 def cmdline_args():
@@ -28,7 +29,7 @@ YELLOW = "\033[0;33m"
 GREEN = "\033[0;32m"
 WHITE = "\033[0;39m"
 
-def chat_loop(pdf_qa):
+def chat_loop(qa_chain):
     chat_history = []
     while True:
         query = input(f"{GREEN}Prompt: ")
@@ -37,7 +38,7 @@ def chat_loop(pdf_qa):
             sys.exit()
         if query == '':
             continue
-        result = pdf_qa(
+        result = qa_chain(
         {"question": query, "chat_history": chat_history})
         print(f"{WHITE}Answer: " + result["answer"])
         chat_history.append((query, result["answer"]))
@@ -66,10 +67,19 @@ if __name__ == '__main__':
     #vectordb.persist()
 
     # create our Q&A chain
+    prompt_template = """Use the following pieces of context to answer the question at the end in the style of the character Tom Servo from MST3k. If you don't know the answer, just say that you don't know, don't try to make up an answer.
+    
+    {context}
+    
+    Question: {question}
+    Helpful Answer:"""
+    prompt = PromptTemplate(input_variables=["context", "question"], template=prompt_template)
+    llm = ChatOpenAI(temperature=0.7, model_name='gpt-3.5-turbo', verbose=args.verbose)
     article_qa = ConversationalRetrievalChain.from_llm(
-        ChatOpenAI(temperature=0.7, model_name='gpt-3.5-turbo', verbose=args.verbose),
+        llm=llm,
         retriever=vectordb.as_retriever(search_kwargs={'k': 6}),
         return_source_documents=True,
+        combine_docs_chain_kwargs={"prompt": prompt},
         verbose=args.verbose
     )
     print(f"{YELLOW}---------------------------------------------------------------------------------")
